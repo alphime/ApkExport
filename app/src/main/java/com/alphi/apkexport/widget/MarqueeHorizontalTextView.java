bb6a7e08d7c8c6dd23fa2c5d97684232bb5a4f79
alphi <alphime@foxmail.com>
2022-07-21 23:58:30 +0800

    Apk Export

--- app/src/main/java/com/alphi/apkexport/widget/MarqueeHorizontalTextView.java
+++ app/src/main/java/com/alphi/apkexport/widget/MarqueeHorizontalTextView.java
@@ -15,12 +15,16 @@ import android.widget.TextView;
 @SuppressLint("AppCompatCustomView")
 public class MarqueeHorizontalTextView extends TextView {
     private float textLength = 0f;
-    private float drawTextX = 0f;// 文本的横坐标
-    public boolean isStarting;// 是否开始滚动
+    // 文本的负横坐标
+    private float drawTextX = 0f;
+    // 滚动布尔值
+    public boolean isScrolling;
     private Paint paint;
     private String text;
-    private final long waitTime = 1000; //开始时等待的时间
-    private final int scrollTile = 1; //文字的滚动速度
+    // 停留时间
+    private final int delayTime = 1000;
+    // 文字的滚动速度
+    private final int scrollSpeed = 1;
 
     public MarqueeHorizontalTextView(Context context) {
         super(context);
@@ -38,14 +42,17 @@ public class MarqueeHorizontalTextView extends TextView {
     }
 
     private void initView() {
+        // 获取文本绘画属性
         paint = getPaint();
+        // 文本颜色
         paint.setColor(getTextColors().getColorForState(getDrawableState(), 0));
         text = getText().toString();
         if (TextUtils.isEmpty(text)) {
             return;
         }
+        // 获取文字的宽度
         textLength = paint.measureText(text);
-        isStarting = true;
+        isScrolling = true;
     }
 
     @Override
@@ -57,44 +64,56 @@ public class MarqueeHorizontalTextView extends TextView {
         start();
     }
 
+    /**
+     * 启用跑马灯
+     */
     public void start() {
-        isStarting = true;
+        isScrolling = true;
         invalidate();
     }
 
+    /**
+     * 停用跑马灯
+     */
     public void stop() {
-        isStarting = false;
+        isScrolling = false;
         invalidate();
     }
 
     @Override
     public void onDraw(Canvas canvas) {
+        // 获取字体度量
         final Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-        int baseline = (getHeight() - fontMetrics.bottom - fontMetrics.top) / 2;        //设置基线
+        //计算基线
+        int baseline = (getHeight() - fontMetrics.bottom - fontMetrics.top) / 2;
         if (textLength <= getWidth()) {
             canvas.drawText(text, 0, baseline, paint);
             return;
         }
         canvas.drawText(text, -drawTextX, baseline, paint);
 
-        if (!isStarting) {
+        if (!isScrolling) {
             return;
         }
         if (drawTextX == 0) {
+            // 当回到原点时，暂停一小会
             postDelayed(() -> {
-                drawTextX = 1;  // 赋值文本的横坐标为 1
-                isStarting = true;
-                invalidate();       // 结束onDraw执行
-            }, waitTime);
-            isStarting = false;
+                // 赋值文本的横坐标为 -1
+                drawTextX = 1;
+                isScrolling = true;
+                // 标记使原始图无效，此时呢，视图就要重绘
+                invalidate();
+            }, delayTime);
+            isScrolling = false;
             return;
         }
-        drawTextX += scrollTile;
+        drawTextX += scrollSpeed;
         //判断是否滚动结束
         if (drawTextX > textLength) {
             // 文本开头在屏幕最右边
             drawTextX = -getWidth();
         }
+        // 重绘视图
         invalidate();
     }
 }
