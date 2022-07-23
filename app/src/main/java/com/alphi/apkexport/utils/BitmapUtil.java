b731cf370dc9aa8d6e66bff7ea8b4833be3e649b
alphi <alphime@foxmail.com>
2022-07-23 12:42:47 +0800

    Apk Export

--- app/src/main/java/com/alphi/apkexport/utils/BitmapUtil.java
+++ app/src/main/java/com/alphi/apkexport/utils/BitmapUtil.java
@@ -11,7 +11,7 @@ public class BitmapUtil {
     public static Bitmap drawableToBitmap150(Drawable drawable) {
         if (drawable != null && drawable.getIntrinsicHeight() > 0 && drawable.getIntrinsicWidth() > 0) {
             Bitmap bitmap = Bitmap.createBitmap(150, 150, Bitmap.Config.ARGB_8888);
-            // 创建画板
+            // 建立 bitmap 画板
             Canvas canvas = new Canvas(bitmap);
             // 设置图像大小
             drawable.setBounds(0, 0, 150, 150);
